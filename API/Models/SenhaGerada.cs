6f523118de56013b3bf8e210f290340276244385
AliA-Mohamad <ali.mohamad.uc@gmail.com>
2023-12-19 23:23:53 -0300

    Adicionar arquivos de projeto.

---
+++ API/Models/SenhaGerada.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace API.Models;
+
+public class SenhaGerada
+{
+    public string identificador { get; set; }
+    public string senha { get; }
+
+
+    public SenhaGerada()
+    {
+        senha = GerarSenhaAleatoria();
+    }
+
+    private string GerarSenhaAleatoria()
+    {
+        Random random = new();
+        StringBuilder stringBuilder = new(8);
+
+        for (int i = 0; i < 8; i++)
+        {
+            int tipoCaractere = random.Next(3);
+
+            switch (tipoCaractere)
+            {
+                case 0:
+                    stringBuilder.Append((char)random.Next('0', '9' + 1));
+                    break;
+                case 1:
+                    stringBuilder.Append((char)random.Next('a', 'z' + 1));
+                    break;
+                case 2:
+                    stringBuilder.Append((char)random.Next('A', 'Z' + 1));
+                    break;
+            }
+        }
+        return stringBuilder.ToString();
+    }
+}
---
+++ API/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models;
+
+public class Usuario
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public List<SenhaGerada> SenhasGeradas { get; set; }
+}
